c8e188c931548d24aed3801487e6f02029a0831a
Iván Manzaba <mauricio.9.inm@gmail.com>
2022-08-16 21:39:09 -0500

    arreglo de errores en el login y en el registro de usuarios

--- server/src/clientes/perfil/perfil.controller.ts
+++ server/src/clientes/perfil/perfil.controller.ts
@@ -2,6 +2,8 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/session/jwt-auth.guard';
+import { Role } from 'src/auth/session/role.enum';
+import { Roles } from 'src/auth/session/roles.decorator';
 import { RolesGuard } from 'src/auth/session/roles.guard';
 import { PerfilService } from './perfil.service';
 
@@ -9,7 +11,7 @@ import { PerfilService } from './perfil.service';
 @Controller('perfil')
 @ApiTags('perfil')
 @UseGuards(JwtAuthGuard, RolesGuard)
-
+@Roles(Role.Cliente)
 export class PerfilController {
     constructor(private servicio:PerfilService) {}
 
