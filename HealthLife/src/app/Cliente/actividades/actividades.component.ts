cdeed4a7c48c7fabbef61a6352aa4029350961b6
jordanfvc26 <jverac12@uteq.edu.ec>
2022-08-29 00:40:34 -0500

    Mostrar actividades y suscripciones

---
+++ HealthLife/src/app/Cliente/actividades/actividades.component.ts
@@ -0,0 +1,32 @@
+import { Connection } from './../../connection';
+import { Component, Input, OnInit } from '@angular/core';
+import * as iconos from '@fortawesome/free-solid-svg-icons';
+import * as AOS from 'aos';
+
+@Component({
+  selector: 'app-actividades',
+  templateUrl: './actividades.component.html',
+  styleUrls: ['./actividades.component.css']
+})
+export class ActividadesComponent implements OnInit {
+
+  @Input() id!:number;
+  actividades:any[] =[]
+
+
+  constructor(private api:Connection) { }
+
+  ngOnInit(): void {
+    //cambiar este /10 por el id traido del plan
+    this.api.get("actividades/obtener_actividades/10").subscribe(res=>{
+      //this.actividades.push(res);
+      const aux = Object.assign(res)
+      aux.forEach((element: any) => {
+        this.actividades.push(element)
+      });
+    })
+  }
+
+  faVerificado = iconos.faCircleCheck;
+
+}
